fd5696a96cc04ce54858448f241992dc53e47f1f
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-09-08 11:57:07 +0900

    Update BrainFuckEx.py

--- BrainFuckEx.py
+++ BrainFuckEx.py
@@ -6,14 +6,15 @@ class BrainFuckEx:
 
     def loopHelper(self, code):
         book = {}
-        q = Deque()
+        q = []
         for i in range(len(code)):
             if code[i] == "[":
-                q.rightappend(i)
+                q.append(i)
             elif code[i] == "]":
-                idx = q.rightpop()
+                idx = q[-1]
                 book[idx] = i
                 book[i] = idx
+                q = q[:-1]
         return book
 
     def Clear(self): self.__init__()
@@ -34,14 +35,14 @@ class BrainFuckEx:
             elif p == "<": v -= 1
             elif p == "v": h += 1
             elif p == "^": h -=1
-            elif p == "+": self.values[v][h] += 1
-            elif p == "-": self.values[v][h] -= 1
-            elif p == "*": self.values[v][h] += self.values[v][0]*self.values[0][h]
+            elif p == "+": self.values[h][v] += 1
+            elif p == "-": self.values[h][v] -= 1
+            elif p == "*": self.values[h][v] += self.values[h][0]*self.values[0][v]
             elif p == "[": pass
             elif p == "]":
-                if self.values[v][h]: x = z[x] - 1
-            elif p == ".": data.append(chr(self.values[v][h]))
-            elif p == ",": self.values[v][h] = ord(input()[0])
+                if self.values[h][v]: x = z[x] - 1
+            elif p == ".": data.append(chr(self.values[h][v]))
+            elif p == ",": self.values[h][v] = ord(input()[0])
             elif p == "=": break;
             else: continue
             x += 1
